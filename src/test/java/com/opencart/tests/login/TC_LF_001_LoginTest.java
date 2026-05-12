1cc036ee6d63b2cd9ea1e79d174d571387fd4171
zohairawan <35776095+zohairawan@users.noreply.github.com>
2026-05-12 11:12:27 -0700

    Refactor - Remove invalid login from TC_LF_001_LoginTest class and put it into its own class

--- src/test/java/com/opencart/tests/login/TC_LF_001_LoginTest.java
+++ src/test/java/com/opencart/tests/login/TC_LF_001_LoginTest.java
@@ -28,25 +28,4 @@ public class TC_LF_001_LoginTest extends BaseTest {
             Assert.fail();
         }
     }
-
-    @Test
-    public void testLoginWithInvalidCredentials() {
-        logger.info("Starting invalid login test");
-        homePage.open();
-        LoginPage loginPage = homePage.goToLoginPage();
-
-        logger.info("Authenticating with invalid credentials");
-        loginPage.loginInvalidUser("wrong@demo.com", "wrongpwd");
-
-        String expectedInvalidUserErrorMsg = loginPage.INVALID_USER_ERROR_MSG;
-        String actualInvalidUserErrorMsg = loginPage.getInvalidUserErrorMsg();
-        logger.info("Validating error message");
-        try {
-            Assert.assertEquals(actualInvalidUserErrorMsg, expectedInvalidUserErrorMsg);
-            logger.info("Invalid login test passed");
-        } catch (AssertionError e) {
-            logger.error("Invalid login test failed: {}", e.getMessage());
-            Assert.fail();
-        }
-    }
 }
---
+++ src/test/java/com/opencart/tests/login/TC_LF_002_LoginTest.java
@@ -0,0 +1,30 @@
+package com.opencart.tests.login;
+
+import com.opencart.pages.LoginPage;
+import com.opencart.tests.base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_LF_002_LoginTest extends BaseTest {
+
+    @Test
+    public void testLoginWithInvalidCredentials() {
+        logger.info("Starting invalid login test");
+        homePage.open();
+        LoginPage loginPage = homePage.goToLoginPage();
+
+        logger.info("Authenticating with invalid credentials");
+        loginPage.loginInvalidUser("wrong@demo.com", "wrongpwd");
+
+        String expectedInvalidUserErrorMsg = loginPage.INVALID_USER_ERROR_MSG;
+        String actualInvalidUserErrorMsg = loginPage.getInvalidUserErrorMsg();
+        logger.info("Validating error message");
+        try {
+            Assert.assertEquals(actualInvalidUserErrorMsg, expectedInvalidUserErrorMsg);
+            logger.info("Invalid login test passed");
+        } catch (AssertionError e) {
+            logger.error("Invalid login test failed: {}", e.getMessage());
+            Assert.fail();
+        }
+    }
+}

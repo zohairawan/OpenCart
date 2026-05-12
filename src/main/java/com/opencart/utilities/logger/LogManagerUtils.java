e618bf886cbe25367b324ac4a13334494dbb6e99
zohairawan <35776095+zohairawan@users.noreply.github.com>
2026-05-11 17:26:08 -0700

    Create logging capabilities

---
+++ src/main/java/com/opencart/utilities/logger/LogManagerUtils.java
@@ -0,0 +1,11 @@
+package com.opencart.utilities.logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogManagerUtils {
+
+    public static Logger getLogger(Class<?> clazz) {
+        return LogManager.getLogger(clazz);
+    }
+}

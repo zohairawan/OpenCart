4ccbf4ce7fc491d030da441fd1ab2736626f6f46
zohairawan <35776095+zohairawan@users.noreply.github.com>
2026-05-15 22:09:18 -0700

    Add reporting to TC_LF_001_LoginTest

--- src/test/java/com/opencart/tests/login/TC_LF_001_LoginTest.java
+++ src/test/java/com/opencart/tests/login/TC_LF_001_LoginTest.java
@@ -1,50 +1,37 @@
 package com.opencart.tests.login;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.opencart.pages.AccountPage;
 import com.opencart.pages.LoginPage;
 import com.opencart.tests.base.BaseTest;
 import com.opencart.utilities.properties.ConfigPropertiesFileReaderUtils;
+import com.opencart.utilities.report.ExtentReportUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 public class TC_LF_001_LoginTest extends BaseTest {
 
     @Test
     public void testLoginWithValidCredentials() {
-        loggerUtil.info("Starting valid login test");
+        ExtentTest extentTest = ExtentReportUtils.getThreadLocalTest();
+        extentTest.info("Login test is starting");
         homePage.open();
         LoginPage loginPage = homePage.goToLoginPage();
 
-        loggerUtil.info("Entering valid credentials");
+        extentTest.info("User logs in with valid credentials");
         AccountPage accountPage = loginPage.loginValidUser(ConfigPropertiesFileReaderUtils.getValidEmail(), ConfigPropertiesFileReaderUtils.getValidPassword());
 
-        loggerUtil.info("Retrieving account page URL");
+        extentTest.info("Getting account page URL");
         String actualAccountPageURL = accountPage.getAccountPageURL();
         String expectedAccountPageURL = accountPage.URL;
-        try {
-            loggerUtil.info("Validating successful login - comparing URL");
-            Assert.assertEquals(actualAccountPageURL, expectedAccountPageURL);
-        } catch (AssertionError e) {
-            loggerUtil.error("Valid login test failed - URL comparison failed: {}", e.getMessage());
-            Assert.fail();
-        }
+        extentTest.info("Validating successful login - comparing URL");
+        Assert.assertEquals(actualAccountPageURL, expectedAccountPageURL);
 
-        loggerUtil.info("Validating successful login - My Account header is displayed");
-        try {
-            Assert.assertTrue(accountPage.isMyAccountHeaderDisplayed());
-        } catch (AssertionError e) {
-            loggerUtil.error("Valid login test failed - My Account header is not displayed: {}", e.getMessage());
-            Assert.fail();
-        }
+        extentTest.info("Validating successful login - My Account header is displayed");
+        Assert.assertTrue(accountPage.isMyAccountHeaderDisplayed());
 
-        loggerUtil.info("Validating successful login - logout link is displayed");
-        try {
-            Assert.assertTrue(accountPage.isLogoutLinkDisplayed());
-        } catch (AssertionError e) {
-            loggerUtil.error("Valid login test failed - Logout link is not displayed: {}", e.getMessage());
-            Assert.fail();
-        }
+        extentTest.info("Validating successful login - logout link is displayed");
+        Assert.assertTrue(accountPage.isLogoutLinkDisplayed());
 
-        loggerUtil.info("Valid login test passed");
+        extentTest.pass("Valid login test passed");
     }
 }

89cb799ccc2b0933096404420f1a0ed5e5b6d294
zohairawan <35776095+zohairawan@users.noreply.github.com>
2026-05-16 11:54:20 -0700

    Add logging

--- src/main/java/com/opencart/utilities/browser/WebDriverSetupUtils.java
+++ src/main/java/com/opencart/utilities/browser/WebDriverSetupUtils.java
@@ -1,13 +1,17 @@
-/**
- * Purpose: Assigns WebDriver to a concrete class by reading the browser type
- * from the config.properties file
+/*
+ * Purpose:
+ * - Assigns WebDriver to a concrete class by reading the browser type
+ *   from the config.properties file
  */
 
 package com.opencart.utilities.browser;
 
 import com.opencart.constants.Constant;
+import com.opencart.utilities.logger.LogManagerUtils;
 import com.opencart.utilities.properties.ConfigPropertiesFileReaderUtils;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import java.lang.annotation.AnnotationTypeMismatchException;
 
 public class WebDriverSetupUtils {
 
@@ -15,10 +19,15 @@ public class WebDriverSetupUtils {
         WebDriver driver;
         String driverType = (browser == null) ? ConfigPropertiesFileReaderUtils.getBrowserValue() : browser;
 
+        Logger loggerUtil = LogManagerUtils.getLogger(WebDriverSetupUtils.class);
+        loggerUtil.info("Initializing browser type to: '{}'", driverType);
         switch (driverType.toLowerCase()) {
             case Constant.CHROME_BROWSER -> driver = ChromeDriverSetupUtils.initializeChromeDriver();
             case Constant.FIREFOX_BROWSER -> driver = FirefoxDriverSetupUtils.initializeFirefoxDriver();
-            case null, default -> throw new RuntimeException("======= INVALID BROWSER TYPE PROVIDED =======");
+            default -> {
+                loggerUtil.error("Invalid browser type: '{}'", driverType);
+                throw new RuntimeException();
+            }
         }
 
         driver.manage().window().maximize();

2671ebd69840ce0e5ab5d96d4ac6f36fef2c9a3a
zohairawan <35776095+zohairawan@users.noreply.github.com>
2026-05-17 10:10:49 -0700

    Refactor - Convert screenshot path from String to Path and update in ScreenshotUtils class

--- src/main/java/com/opencart/utilities/screencapture/ScreenshotUtils.java
+++ src/main/java/com/opencart/utilities/screencapture/ScreenshotUtils.java
@@ -2,9 +2,13 @@ package com.opencart.utilities.screencapture;
 
 import com.opencart.constants.Constant;
 import com.opencart.utilities.browser.DriverManagerUtils;
+import com.opencart.utilities.logger.LogManagerUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,7 +18,14 @@ public class ScreenshotUtils {
         TakesScreenshot screenshot = (TakesScreenshot) DriverManagerUtils.getDriver();
         File screenshotSourceFile = screenshot.getScreenshotAs(OutputType.FILE);
         String screenshotTimestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
-        File screenshotTargetFile = new File(Constant.SCREENSHOTS_FILE_PATH + screenshotTimestamp + ".png");
-        screenshotSourceFile.renameTo(screenshotTargetFile);
+        Path screenshotTargetPath = Constant.SCREENSHOTS_FILE_PATH.resolve(screenshotTimestamp + ".png");
+        try {
+            Files.copy(screenshotSourceFile.toPath(), screenshotTargetPath);
+        } catch (IOException e) {
+            LogManagerUtils.getLogger(ScreenshotUtils.class).error(
+                    "Unable to store screenshot, " +
+                            "invalid path to screenshots directory {}",
+                    screenshotTargetPath.toString());
+        }
     }
 }

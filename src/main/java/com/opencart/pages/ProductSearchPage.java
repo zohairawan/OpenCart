79e84384fcbba29dc7990dca87d80331f0ab8bdb
zohairawan <35776095+zohairawan@users.noreply.github.com>
2026-06-03 10:44:30 -0700

    Add ProductSearchPage class

---
+++ src/main/java/com/opencart/pages/ProductSearchPage.java
@@ -0,0 +1,11 @@
+package com.opencart.pages;
+
+import com.opencart.pages.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class ProductSearchPage extends BasePage {
+
+    public ProductSearchPage(WebDriver driver) {
+        super(driver);
+    }
+}
